74d882ada9312a1fab6f57752dc17a82998e3528
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-07-13 17:22:16 +0530

    delete functionality implemented

--- script.js
+++ script.js
@@ -23,18 +23,67 @@ function handleSubmit(e){
     let ul = document.getElementById('users')
     let li = document.createElement('li')
     li.className='item'
-    li.textContent = `${arr.length} Name : ${data.name} Email : ${data.email}`
+    let span = document.createElement('span')
+    span.textContent = `${arr.length} Name : ${data.name} Email : ${data.email}`
+    li.appendChild(span) 
+    let div = document.createElement('div')
+
+    let edit = document.createElement('button')
+    // edit.className = 'btn btn-primary btn-sm mx-2'
+    edit.textContent = "edit"
+    div.appendChild(edit)
+    let deleteBtn = document.createElement('button')
+    deleteBtn.className = 'delete'
+    deleteBtn.textContent ='X'
+    div.appendChild(deleteBtn)
+    li.appendChild(div)
     ul.appendChild(li)
 }
-
+{/* <div class="float-right">
+            <button type="button" class="btn btn-primary btn-sm mx-2">edit</button><button class="btn btn-danger btn-sm delete">X</button>
+          </div> */}
 function renderElements(){
     const users =JSON.parse(localStorage.getItem('users'))
     let ul = document.getElementById('users')
-    console.log("console")
+    ul.innerHTML = ``
     users.forEach( (user ,index) => {
         let li = document.createElement('li')
         li.className='item'
-        li.textContent = `${index+1} Name : ${user.name} Email : ${user.email}`
+        let span = document.createElement('span')
+        span.textContent = `${index+1} Name : ${user.name} Email : ${user.email}`
+        li.appendChild(span) 
+        let div = document.createElement('div')
+
+        let edit = document.createElement('button')
+        // edit.className = 'btn btn-primary btn-sm mx-2'
+        edit.textContent = "edit"
+        div.appendChild(edit)
+        let deleteBtn = document.createElement('button')
+        deleteBtn.className = 'delete'
+        deleteBtn.textContent ='X'
+        div.appendChild(deleteBtn)
+        li.appendChild(div)
         ul.appendChild(li)
     });
 }
+
+var dl = document.getElementById('users')
+dl.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('delete')){
+        let index = e.target.parentNode.parentNode.textContent[0] -1
+        const users =JSON.parse(localStorage.getItem('users'))
+        let ul = document.getElementById('users')
+
+        if(users.length === 1 && index === 0){
+            localStorage.removeItem('users')
+            ul.removeChild(e.target.parentNode.parentNode)
+        }
+        else{
+            users.splice(index ,1)
+            localStorage.setItem('users' , JSON.stringify(users))
+            renderElements()
+        }
+
+
+    }
+})
