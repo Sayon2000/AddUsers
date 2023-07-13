f29cab7a9199b14a6c2b6849b2b565d49296578e
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-07-13 16:24:58 +0530

    displaying users

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let form = document.getElementById('my-form')
 
 form.addEventListener('submit' , handleSubmit)
+window.addEventListener('load' , ()=>{
+    if(localStorage.getItem('users') !== null)
+    renderElements()
+})
 
 function handleSubmit(e){
     e.preventDefault()
@@ -16,5 +20,21 @@ function handleSubmit(e){
     localStorage.setItem('users' ,JSON.stringify( arr));
     e.target.name.value =""
     e.target.email.value =""
+    let ul = document.getElementById('users')
+    let li = document.createElement('li')
+    li.className='item'
+    li.textContent = `${arr.length} Name : ${data.name} Email : ${data.email}`
+    ul.appendChild(li)
+}
 
+function renderElements(){
+    const users =JSON.parse(localStorage.getItem('users'))
+    let ul = document.getElementById('users')
+    console.log("console")
+    users.forEach( (user ,index) => {
+        let li = document.createElement('li')
+        li.className='item'
+        li.textContent = `${index+1} Name : ${user.name} Email : ${user.email}`
+        ul.appendChild(li)
+    });
 }
