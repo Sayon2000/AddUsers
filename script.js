2988940f019b2af94a9522481007f2432fb4d12d
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-07-13 06:24:32 +0530

    adding users to localStorage

---
+++ script.js
@@ -0,0 +1,18 @@
+let form = document.getElementById('my-form')
+
+form.addEventListener('submit' , handleSubmit)
+
+function handleSubmit(e){
+    e.preventDefault()
+    let arr = []
+    if(localStorage.getItem('users') !== null)
+        arr = JSON.parse(localStorage.getItem('users') )
+    
+    const data = {name : e.target.name.value , 
+                email : e.target.email.value
+        }
+
+    arr.push(data)
+    localStorage.setItem('users' ,JSON.stringify( arr));
+
+}
